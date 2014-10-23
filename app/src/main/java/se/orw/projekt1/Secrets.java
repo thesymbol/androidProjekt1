88831f39eaea69ca492aa2a0ffd098215809bd79
Salti <viktorsaltarski@live.se>
2014-10-23 19:51:16 +0200

    Nycklarna tillagda...

--- app/src/main/java/se/orw/projekt1/Secrets.java
+++ app/src/main/java/se/orw/projekt1/Secrets.java
@@ -4,6 +4,6 @@ package se.orw.projekt1;
  * Created by Marcus on 2014-10-23.
  */
 public class Secrets {
-    public static final String CONSUMER_KEY = "";
-    public static final String CONSUMER_SECRET = "";
+    public static final String CONSUMER_KEY = "PBLcuezecqL3nckapucu3S72q";
+    public static final String CONSUMER_SECRET = "LMweNP0swupBUnvOni4a7Iwf4Cm9Ownr1bzgnsFeaKbOldxhDV";
 }
