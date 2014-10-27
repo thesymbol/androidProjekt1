7c2b245067f68693d491ebfe094431d87c7026bf
Marcus <marcus.orwen@gmail.com>
2014-10-27 15:21:10 +0100

    Added swedish language and some strings.xml values. Added Navigation drawer to the left side.

--- app/src/main/java/se/orw/projekt1/Controller.java
+++ app/src/main/java/se/orw/projekt1/Controller.java
@@ -4,19 +4,34 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.res.Configuration;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
 import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import se.orw.projekt1.Twitter.TwitterController;
 import se.orw.projekt1.Twitter.TwitterFragment;
 import se.orw.projekt1.Twitter.TwitterFunctions;
 
 /**
+ * Controller for app
+ *
  * Created by Marcus on 2014-10-22.
  */
+@SuppressWarnings("deprecation")
 public class Controller {
     private Activity activity;
     private TestFragment testFragment;
     private TwitterFragment twitterFragment;
+
+    private ActionBarDrawerToggle drawerToggle;
+    private DrawerLayout drawerLayout;
+    private ListView drawerList;
 
     public Controller(Activity activity) {
         this.activity = activity;
@@ -28,6 +43,7 @@ public class Controller {
         twitterFragment.setController(this);
 
         switchToFragment(testFragment, null);
+        initNavigationDrawer();
     }
 
     public void twitterConnect() {
@@ -64,5 +80,55 @@ public class Controller {
             transaction.replace(R.id.activity_main, fragment);
         }
         transaction.commit();
+    }
+
+    /**
+     * Used to sync the state of the button
+     */
+    public void onPostCreate() {
+        drawerToggle.syncState();
+    }
+
+    /**
+     * Once the app changes configuration (eg rotation).
+     *
+     * @param newConfig The config
+     */
+    public void onConfigurationChanged(Configuration newConfig) {
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    /**
+     * Handles the drawer on the left sides button
+     *
+     * @param item Item in the menu
+     * @return true if item is selected else false
+     */
+    public boolean onDrawerToggle(MenuItem item) {
+        return drawerToggle.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Initialize the navigation drawer
+     */
+    private void initNavigationDrawer() {
+        String[] menu = {"List item 1", "List item 2"};
+        drawerLayout = (DrawerLayout) activity.findViewById(R.id.layout_drawer);
+        drawerList = (ListView) activity.findViewById(R.id.left_drawer);
+
+        drawerList.setAdapter(new ArrayAdapter<String>(activity, R.layout.drawer_list_item, menu));
+        drawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+
+                drawerLayout.closeDrawers();
+            }
+        });
+        drawerToggle = new ActionBarDrawerToggle(activity, drawerLayout, R.drawable.ic_drawer, R.string.drawerOpen, R.string.drawerClose);
+        drawerLayout.setDrawerListener(drawerToggle);
+        if(activity.getActionBar() != null) {
+            activity.getActionBar().setDisplayHomeAsUpEnabled(true);
+            activity.getActionBar().setHomeButtonEnabled(true);
+        }
     }
 }
--- app/src/main/java/se/orw/projekt1/MainActivity.java
+++ app/src/main/java/se/orw/projekt1/MainActivity.java
@@ -1,6 +1,7 @@
 package se.orw.projekt1;
 
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -26,13 +27,18 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+        return controller.onDrawerToggle(item) || super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        controller.onPostCreate();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        controller.onConfigurationChanged(newConfig);
     }
 }
