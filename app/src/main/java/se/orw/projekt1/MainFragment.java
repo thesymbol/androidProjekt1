1c4a82eb06938aaa1d6494857c980f3b7e26c670
Matte <RH_Monztor@hotmail.com>
2014-10-28 20:40:12 +0100

    Changed the MainFragment. Added som background images and checkboxes and updated everything.

--- app/src/main/java/se/orw/projekt1/MainFragment.java
+++ app/src/main/java/se/orw/projekt1/MainFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -15,7 +16,7 @@ import android.widget.TextView;
  *
  */
 public class MainFragment extends Fragment {
-    private Button btnTranslate;
+    private CheckBox cbFacebook, cbTwitter, cbGoogle;
     private EditText etTextBox;
     private TextView tvPhrase;
     private Controller controller;
@@ -24,7 +25,7 @@ public class MainFragment extends Fragment {
         // Required empty public constructor
     }
 
-    public void setController(){
+    public void setController() {
         this.controller = controller;
     }
 
@@ -32,18 +33,11 @@ public class MainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_main, container, false);
-        btnTranslate = (Button)view.findViewById(R.id.btnTranslate);
-        etTextBox = (EditText)view.findViewById(R.id.etTextBox);
-        tvPhrase = (TextView)view.findViewById(R.id.tvPhrase);
-        btnTranslate.setOnClickListener(new ButtonclickListener());
+        cbFacebook = (CheckBox) view.findViewById(R.id.cbFacebook);
+        cbTwitter = (CheckBox) view.findViewById(R.id.cbTwitter);
+        cbGoogle = (CheckBox) view.findViewById(R.id.cbGoogle);
+        etTextBox = (EditText) view.findViewById(R.id.etTextBox);
+        tvPhrase = (TextView) view.findViewById(R.id.tvPhrase);
         return view;
-    }
-
-
-    private class ButtonclickListener implements View.OnClickListener {
-        @Override
-        public void onClick(View v) {
-            //           controller.
-        }
     }
 }
